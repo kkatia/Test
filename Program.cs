7975bf263fabffcac1ec11a545cacad612eed2ab
koekak <36029841+koekak@users.noreply.github.com>
2018-10-11 16:57:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    class Reverse
+    {        
+      public void Reversing(string a)
+        {
+            char[] ar = a.ToArray<char>();
+
+            for (int j = ar.Length - 1; j >= 0; j--)
+            {
+                Console.Write(ar[j]);
+            }
+            Console.WriteLine();
+        }
+            
+    }
+
+    class Program
+    {
+                static void Main()
+        {
+            string a="";
+            Console.WriteLine("Input a string and get the reversed string: "+
+                "\n Input 'exit' to quit the application.");
+            while (a != "exit")
+            {
+                a = Console.ReadLine();
+                Reverse re = new Reverse();
+                re.Reversing(a);
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
